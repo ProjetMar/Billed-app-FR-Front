8fcf1b10634a156659401747793fef0a1687a88c
marwa mezghani <marwamezghani29@gmail.com>
2024-07-22 11:59:22 +0200

    ajout de test pour augmenter avoir  le rapport de couverture du fichier container/Bills en jaune avec un rapport de couverture plus que 70 % en ajoutant test pour récupération des factures depuis l'API et des tests pour  vérifier comment l'application gère les erreurs lors de la récupération des factures depuis une API mockée

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-
+import '@testing-library/jest-dom'
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+//ajout ca
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
+
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -26,7 +30,26 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
+      expect(windowIcon).toHaveClass('active-icon')
 
+    })
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+
+      await waitFor(()=>screen.getByTestId('btn-new-bill'))
+      const NewBillBtn = screen.getByTestId('btn-new-bill')
+      expect(NewBillBtn).toHaveTextContent("Nouvelle note de frais")
+
+      await waitFor(() => screen.getByTestId('tbody'))
+      const tbody = screen.getByTestId('tbody')
+      expect(tbody).toBeInTheDocument()
+      expect(tbody.querySelectorAll('tr').length).toBeGreaterThan(0)
+      
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
@@ -35,5 +58,52 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        Object.defineProperty(
+            window,
+            'localStorage',
+            { value: localStorageMock }
+        )
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'employee',
+          email: "a@a"
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        router()
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }})
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+  
+      test("fetches messages from an API and fails with 500 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+  
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+
   })
 })
